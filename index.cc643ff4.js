a550628c5873706cbb9752d97b438d394b1d91aa
milacherkashyna <113541226+milacherkashyna@users.noreply.github.com>
2023-03-01 13:51:08 +0000

    Deploying to gh-pages from @ milacherkashyna/goit-js-hw-10@29b87aa5a38350c7586a43a2e6a0972df91d52e5 🚀

--- index.cc643ff4.js
+++
@@ -1,2 +0,0 @@
-!function(){var n;(n="Germany",void fetch("https://restcountries.com/v3.1/name/".concat(n)).then((function(n){if(!n.ok)throw new Error(n.status);return n.json()}))).then(console.log)}();
-//# sourceMappingURL=index.cc643ff4.js.map
--- index.ff4dbfcf.js
+++
@@ -1,2 +0,0 @@
-var t;(t="Germany",void fetch(`https://restcountries.com/v3.1/name/${t}`).then((t=>{if(!t.ok)throw new Error(t.status);return t.json()}))).then(console.log);
-//# sourceMappingURL=index.ff4dbfcf.js.map
