29b87aa5a38350c7586a43a2e6a0972df91d52e5
milacherkashyna <mila.cherkashyna@gmail.com>
2023-03-01 15:50:35 +0200

    commit 3

--- src/index.js
+++ src/index.js
@@ -1,5 +1,44 @@
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 import { fetchCountries } from './js/fetchCountries';
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
-fetchCountries('Germany').then(console.log);
+const inputRef = document.querySelector('input#search-box');
+const countriesRef = document.querySelector('.country-list');
+const countryRef = document.querySelector('.country-info');
+
+inputRef.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
+function onInput(event) {
+  countriesRef.innerHTML = '';
+  countryRef.innerHTML = '';
+  const name = event.target.value.trim();
+  if (!name) {
+    return;
+  }
+  fetchCountries(name)
+    .then(result => {
+      if (result.length > 10) {
+        Notiflix.Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+      } else if (result.length > 1) {
+        renderCountriesList(result);
+      } else {
+        renderCountryInfo(result[0]);
+      }
+    })
+    .catch(error =>
+      Notiflix.Notify.failure('Oops, there is no country with that name')
+    );
+}
+function renderCountriesList(array) {
+  const markup = array
+    .map(
+      element =>
+        `<li class="country__item"><img src="${element.flags.svg}" width="80" higth="60"><h2>${element.name.official}<\h2><\li>`
+    )
+    .join('');
+  countriesRef.innerHTML = markup;
+}
+// input#search-box
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,5 +1,7 @@
 export function fetchCountries(name) {
-  fetch(`https://restcountries.com/v3.1/name/${name}`).then(response => {
+  return fetch(
+    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+  ).then(response => {
     if (!response.ok) {
       throw new Error(response.status);
     }
